3b7216b54ed778ac8a7e24ac1d886fbc17ce2fce
Christoph Gruener <christophgruener@me.com>
2019-02-14 19:46:37 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,1 +1,16 @@
-# Add  code here!
+require 'pry'
+
+def prime?(number)
+  if number < 2 
+    false 
+    elsif number > 2 
+      range = (2..(number-1)).to_a
+      range.each do |numbers|
+        if number % numbers == 0 
+          false 
+        else
+          true 
+        end
+    end
+  end
+end
