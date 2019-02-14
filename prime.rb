3b36653068bf15e33bdc3479274a68696a8f4adb
Christoph Gruener <christophgruener@me.com>
2019-02-14 19:49:32 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -4,8 +4,9 @@ def prime?(number)
   range = (2..number-1).to_a
   if number < 2 
     false 
-    elsif range.each {|integer| number % integer
-        end
-    end
+    elsif range.each {|integer| number % integer}.include?(0)
+      false 
+    else 
+      true 
   end
 end
